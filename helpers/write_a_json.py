6a3b028876f85cec4f8399231ec43ed6f44bf0b8
Bruno <bruno.3av@gmail.com>
2022-01-03 23:09:15 -0300

    update writeAJson function

--- helpers/write_a_json.py
+++ helpers/write_a_json.py
@@ -1,22 +1,15 @@
 import json
+import os
 from bson import json_util
 from datetime import datetime
 
 
-def writeAJsonWithTime(data, name: str):
-    parsed_json = json.loads(json_util.dumps(data))
-
-    now = datetime.now()
-    dt_string = now.strftime("%H-%M-%S")
-
-    with open(f"./json/{name} {dt_string}.json", 'w') as json_file:
-        json.dump(parsed_json, json_file,
-                  indent=4,
-                  separators=(',', ': '))
-
-
 def writeAJson(data, name: str):
     parsed_json = json.loads(json_util.dumps(data))
+
+    if not os.path.isdir("./json"):
+        os.makedirs("./json")
+        
 
     with open(f"./json/{name}.json", 'w') as json_file:
         json.dump(parsed_json, json_file,
